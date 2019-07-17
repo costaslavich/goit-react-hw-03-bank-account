7716a5fccef5204af49f0fc66fa7d4afb65829d0
Konstantin Chernykh <costaslavich@gmail.com>
2019-07-17 12:49:31 +0300

    Updates

---
+++ precache-manifest.ef845f1d9d2bb7bfc222f8b96a507143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cba925dc3ade9bc3d59c04c1b377c6f",
+    "url": "/goit-fe-course/index.html"
+  },
+  {
+    "revision": "0a22da26ccbb212e953b",
+    "url": "/goit-fe-course/static/css/main.bde1a4bb.chunk.css"
+  },
+  {
+    "revision": "9b0de42c61e72b817e09",
+    "url": "/goit-fe-course/static/js/2.85d48f9c.chunk.js"
+  },
+  {
+    "revision": "0a22da26ccbb212e953b",
+    "url": "/goit-fe-course/static/js/main.a41477e8.chunk.js"
+  },
+  {
+    "revision": "6fc20f7e957d07e7940e",
+    "url": "/goit-fe-course/static/js/runtime~main.e6ce5aa7.js"
+  }
+]);
